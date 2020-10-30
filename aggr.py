6855baa564e93d86fefebc4e2a3216e39d10b1d3
Anshul Tomar <visiocitytomar@gmail.com>
2020-10-30 15:58:01 +0530

    Complete dataset

--- aggr.py
+++ aggr.py
@@ -6,7 +6,8 @@ import math
 import numpy as np
 from sklearn.metrics import average_precision_score
 
-filename = "fb3k.csv"
+# filename = "data/fb-wson/fb3k.csv"
+filename = "data/fb-wson/fb_correct.csv"
 random.seed(0)
 edge_list = []
 test_fraction = 0.2
@@ -17,20 +18,24 @@ def create_scores_common_neigh(graph,edge_list=None):
         value = len(list(nx.common_neighbors(graph,u,v)))
         yield (u,v,value)
 
+
+num_nodes = 0
 with open(filename,'r') as f:
     lines = f.readlines()
     for line in lines:
         if line.rstrip() == "":
             break
         line = list(map(int,line.rstrip().split(" ")))
+        num_nodes = max(num_nodes,line[0],line[1])
         edge_list.append((line[0],line[1],line[2]))
     
+print(f'Num nodes: {num_nodes} Num edges: {len(edge_list)}')
 edge_list = sorted(edge_list,key=lambda x: x[2])
 train_len = int((1-test_fraction)*len(edge_list))
 train_edge_list = list(map(lambda x:(x[0],x[1]),edge_list[:train_len]))
 test_edge_list = list(map(lambda x:(x[0],x[1]),edge_list[train_len:]))
 g = nx.Graph()
-g.add_nodes_from(list(range(3000)))
+g.add_nodes_from(range(num_nodes))
 g.add_edges_from(train_edge_list)
 non_edge_list = list(set(list(nx.non_edges(g))) - set(test_edge_list))
 test_non_edge_list = sample(non_edge_list,len(non_edge_list)*test_fraction)
